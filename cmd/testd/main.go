676ed1043f5c14f9d03c3117f06604f826343ca0
Developer Experience team at Ignite <hello@ignite.com>
2025-05-24 15:51:41 +0700

    Initialized with Ignite CLI

---
+++ cmd/testd/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	clienthelpers "cosmossdk.io/client/v2/helpers"
+	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
+
+	"github.com/AnhPhix3405/test/app"
+	"github.com/AnhPhix3405/test/cmd/testd/cmd"
+)
+
+func main() {
+	rootCmd := cmd.NewRootCmd()
+	if err := svrcmd.Execute(rootCmd, clienthelpers.EnvPrefix, app.DefaultNodeHome); err != nil {
+		fmt.Fprintln(rootCmd.OutOrStderr(), err)
+		os.Exit(1)
+	}
+}
